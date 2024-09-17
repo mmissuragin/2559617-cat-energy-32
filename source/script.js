c951f154f90ddbd70ba93453bd09f22e370a702e
Maksim <mmissuragin@gmail.com>
2024-09-17 22:25:46 +0300

    стилизация

--- source/script.js
+++
@@ -1,8 +0,0 @@
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', () => {
-  navMain.classList.toggle('main-nav--');
-});
